8e15e42b17603652e65c554b14e714965d4d43b1
Minko Staev <minkostaev@yahoo.com>
2024-01-03 00:44:42 +0200

    feet: v1.0.8 topBr

--- BlazorRadzenMls/Program.cs
+++ BlazorRadzenMls/Program.cs
@@ -13,10 +13,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped<AppState>();
-
 // https://akmultilanguages.azurewebsites.net/
 builder.Services.AddLanguageContainer<EmbeddedResourceKeysProvider>(Assembly.GetExecutingAssembly(), "Languages");
 //builder.Services.AddLanguageContainerFromFolder("Languages", CultureInfo.GetCultureInfo("en-US"));
+
+builder.Services.AddScoped<AppState>();
 
 await builder.Build().RunAsync();
