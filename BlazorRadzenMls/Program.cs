61642226f3cd422fb5559f89490b181549c0edae
Minko Staev <minkostaev@yahoo.com>
2024-01-04 00:19:37 +0200

    feat: v1.0.9 lng working

--- BlazorRadzenMls/Program.cs
+++ BlazorRadzenMls/Program.cs
@@ -1,17 +1,21 @@
-using AKSoftware.Localization.MultiLanguages.Providers;
 using AKSoftware.Localization.MultiLanguages;
+using AKSoftware.Localization.MultiLanguages.Providers;
+using Radzen;
 using BlazorRadzenMls;
 using BlazorRadzenMls.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Reflection;
-using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//https://blazor.radzen.com/get-started
+//this is required for: Dialog, Notification, ContextMenu and Tooltip
+builder.Services.AddRadzenComponents();
 
 // https://akmultilanguages.azurewebsites.net/
 builder.Services.AddLanguageContainer<EmbeddedResourceKeysProvider>(Assembly.GetExecutingAssembly(), "Languages");
--- BlazorRadzenMls/Services/AppState.cs
+++ BlazorRadzenMls/Services/AppState.cs
@@ -4,12 +4,9 @@ using System;
 
 public class AppState
 {
-    public string Language { get; set; } = "en";
+    public event EventHandler? RefreshEvent;
+    public void RefreshPage(string e) { RefreshEvent?.Invoke(e, EventArgs.Empty); }
 
-    //public event EventHandler? RefreshRequestEvent;
-    //public void RefreshPage(string e)
-    //{
-    //    RefreshRequestEvent?.Invoke(e, EventArgs.Empty);
-    //}
+    //public string Language { get; set; } = "en";
 
 }
