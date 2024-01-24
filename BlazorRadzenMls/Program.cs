c4994785e3a74e9be1ad2e1d24ddac86614fd6c5
Minko Staev <minkostaev@yahoo.com>
2024-01-25 00:11:55 +0200

    feat: v1.0.23 Authentication Auth0

--- BlazorRadzenMls/Program.cs
+++ BlazorRadzenMls/Program.cs
@@ -6,11 +6,22 @@ using System.Globalization;
 using System.Reflection;
 using AKSoftware.Localization.MultiLanguages;
 using Radzen;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+    //options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:ClientId"]);
+});
+
+//builder.Services.AddHttpClient("ServerAPI",
+//    client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+//    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // https://blazor.radzen.com/
