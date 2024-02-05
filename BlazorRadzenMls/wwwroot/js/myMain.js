ae919c77d57161f935c43080aab7fdb0b23da1b6
Minko Staev <minkostaev@yahoo.com>
2024-02-05 10:43:12 +0200

    feat: loaders and publish improvements

--- BlazorRadzenMls/wwwroot/js/myMain.js
+++ BlazorRadzenMls/wwwroot/js/myMain.js
@@ -1,11 +1,30 @@
+addLoaderCss();
+gitHub('/Blazor.Radzen.Mls/');// pass your repo name here
+
+function gitHub(addAfterDomain) {// GitHub method to help with publish
+    let base = document.head.getElementsByTagName('base')[0];
+    if (base.href.includes('.github.io')) {
+        base.href = base.href + addAfterDomain;// github domain fix
+    }
+}
+
+function addLoaderCss() {
+    const num = Math.floor((Math.random() * 8) + 1);
+    const linkLoader = document.createElement('link');
+    linkLoader.rel = 'stylesheet';
+    linkLoader.href = `css/loaders/load${num}.css`;
+    document.head.appendChild(linkLoader);
+}
 
 async function reload() {
     const keys = await caches.keys();
+    const i = keys.length;// for debug (delete)
     for (let cch of keys) {
         await caches.delete(cch);
     }
     location.replace(location.href);//origin
     //window.location.reload(true);
+    console.log(i);// for debug (delete)
 }
 
 async function getVersion() {
