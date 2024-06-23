69003538a37dff659b0d505f64d8f2de93aac3f6
Minko Staev <minkostaev@yahoo.com>
2024-06-23 15:59:43 +0300

    feat: v1.0.83 imot form

--- BlazorRadzenMls/Models/Imot.cs
+++ BlazorRadzenMls/Models/Imot.cs
@@ -7,8 +7,10 @@ public class Imot
     public string? Label { get; set; }
     public string? Location { get; set; }
     public string? Type { get; set; }
+    public string? Agent { get; set; }
     public string? Link { get; set; }
     public int Price { get; set; }
+    public string[]? Pictures { get; set; }
     [JsonIgnore]
     public string? Domain => new Uri(Link!).Host;
 }
