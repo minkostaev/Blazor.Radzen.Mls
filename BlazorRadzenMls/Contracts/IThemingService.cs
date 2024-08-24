e7e9894c6705e46b05f7f8b3788e0b1ce83618de
Minko Staev <minkostaev@yahoo.com>
2024-08-25 01:30:45 +0300

    feat: v1.0.123 radzen

--- BlazorRadzenMls/Contracts/IThemingService.cs
+++ BlazorRadzenMls/Contracts/IThemingService.cs
@@ -7,8 +7,8 @@ public interface IThemingService
     string[] ThemesAll { get; }
     string[] ThemesBasic { get; }
     bool? IsDark { get; set; }
-    Task<string?> GetTheme(StateService? state = null);
+    Task<string?> GetTheme(StateService? __state = null);
     Task<bool> SetTheme(string? name, StateService? __state = null, bool saveLocal = false);
-    Task UseIsDarkLightProperty(StateService? state = null);
+    Task UseIsDarkLightProperty(StateService? __state = null);
     string CheckForDarkTheme(string themeName);
 }
