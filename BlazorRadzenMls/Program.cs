857ce57f9b27d736c8294a558dc85fcf2e4d2aa4
Minko Staev <minkostaev@yahoo.com>
2025-02-13 15:08:12 +0200

    feat: v1.1.2 radzen nuget update

--- BlazorRadzenMls/Program.cs
+++ BlazorRadzenMls/Program.cs
@@ -1,5 +1,6 @@
 using BlazorRadzenMls;
 using BlazorRadzenMls.Extensions;
+using BlazorRadzenMls.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,7 +8,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.AddAuthentication();//extension
+builder.Services.AddOidcAuthentication(options =>
+{
+    string authConfig = "Authentication:" + AppStatic.GetAuth0(builder.HostEnvironment.BaseAddress);
+    builder.Configuration.Bind(authConfig, options.ProviderOptions);//"Auth0"
+    options.ProviderOptions.ResponseType = "code";
+    ///options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:ClientId"]);
+});
+
+//builder.AddAuthentication();//extension
 builder.AddHttpClients();//extension
 
 builder.Services.AddExternalLibraries();//extension
