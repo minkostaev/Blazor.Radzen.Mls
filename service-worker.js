239f181605f1b8968ca7de57db8d754cc9d00829
Minko Staev <minkostaev@yahoo.com>
2025-02-18 00:11:58 +0200

    chore: new publish

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: Uir9fGFo */
+/* Manifest version: 3gPeQjOB */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
